083093ef0f764f3779ff572bf9344fde8e27c812
Javier Suárez <javiersuarezruiz@hotmail.com>
2022-12-19 11:21:46 +0100

    Updated sample

--- src/MauiTestingPlayground/Tests/EntryTest.xaml.cs
+++ src/MauiTestingPlayground/Tests/EntryTest.xaml.cs
@@ -1,3 +1,6 @@
+using System.ComponentModel;
+using System.Windows.Input;
+
 namespace MauiTestingPlayground.Tests;
 
 public partial class EntryTest : ContentPage
@@ -5,7 +8,9 @@ public partial class EntryTest : ContentPage
 	public EntryTest()
 	{
 		InitializeComponent();
-	}
+
+        BindingContext = new EntryViewModel();
+    }
 
     void OnClearTextButtonClicked(object sender, EventArgs e)
     {
@@ -27,8 +32,48 @@ public partial class EntryTest : ContentPage
         En9.Text += " more text -";
     }
 
-    private void OnUpdateCursorPositionButtonClicked(object sender, EventArgs e)
+    void OnUpdateCursorPositionButtonClicked(object sender, EventArgs e)
     {
         En16.CursorPosition++;
     }
+
+    void OnEntryTextChanged(object sender, TextChangedEventArgs e)
+    {
+        DisplayAlert("Entry", $"{e.NewTextValue}", "OK");
+    }
+
+    void OnEntryCompleted(object sender, EventArgs e)
+    {
+        DisplayAlert("Entry", "Completed", "OK");
+    }
+}
+
+public class EntryViewModel : INotifyPropertyChanged
+{
+    double number = 1;
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    public EntryViewModel()
+    {
+        ReturnCommand = new Command(() => Number++);
+    }
+
+    public double Number
+    {
+        set
+        {
+            if (number != value)
+            {
+                number = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Number"));
+            }
+        }
+        get
+        {
+            return number;
+        }
+    }
+
+    public ICommand ReturnCommand { private set; get; }
 }
